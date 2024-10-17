5293e5c3c30aaf9027c1a816ca23a4d750209647
Samuel Wei Ze Lim <74489037+samwzlim@users.noreply.github.com>
2024-10-17 20:46:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
                         help='Weight for style reconstruction loss')
     parser.add_argument('--lambda_ds', type=float, default=1,
                         help='Weight for diversity sensitive loss')
-    parser.add_argument('--ds_iter', type=int, default=100000,
+    parser.add_argument('--ds_iter', type=int, default=5000,
                         help='Number of iterations to optimize diversity sensitive loss')
     parser.add_argument('--w_hpf', type=float, default=1,
                         help='weight for high-pass filtering')
@@ -112,7 +112,7 @@ if __name__ == '__main__':
     # training arguments
     parser.add_argument('--randcrop_prob', type=float, default=0.5,
                         help='Probabilty of using random-resized cropping')
-    parser.add_argument('--total_iters', type=int, default=100000,
+    parser.add_argument('--total_iters', type=int, default=5000,
                         help='Number of total iterations')
     parser.add_argument('--resume_iter', type=int, default=0,
                         help='Iterations to resume training/testing')
@@ -174,9 +174,9 @@ if __name__ == '__main__':
 
     # step size
     parser.add_argument('--print_every', type=int, default=10)
-    parser.add_argument('--sample_every', type=int, default=5000)
-    parser.add_argument('--save_every', type=int, default=10000)
-    parser.add_argument('--eval_every', type=int, default=50000)
+    parser.add_argument('--sample_every', type=int, default=1000)
+    parser.add_argument('--save_every', type=int, default=1000)
+    parser.add_argument('--eval_every', type=int, default=1000)
 
     args = parser.parse_args()
     main(args)

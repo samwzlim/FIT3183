359c276075343cbdbd78eb5cba59389bca2ae108
Samuel Wei Ze Lim <74489037+samwzlim@users.noreply.github.com>
2024-10-17 20:29:58 +0800

    Update main.py batch size

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
                         help='Number of total iterations')
     parser.add_argument('--resume_iter', type=int, default=0,
                         help='Iterations to resume training/testing')
-    parser.add_argument('--batch_size', type=int, default=4,
+    parser.add_argument('--batch_size', type=int, default=16,
                         help='Batch size for training')
     parser.add_argument('--val_batch_size', type=int, default=32,
                         help='Batch size for validation')
